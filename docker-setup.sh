61bb025f3e14e0f63565bc608b3e0833f9bca104
Raghu K <raghuk.vit@gmail.com>
2018-09-02 11:47:37 +0000

    Updating proj-jenkins

--- docker-setup.sh
+++ docker-setup.sh
@@ -6,5 +6,5 @@ sudo usermod -a -G docker $USER
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo curl -s -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 sudo su - $USER
